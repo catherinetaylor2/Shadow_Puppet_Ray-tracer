995fe391066929a85b002700c8f0b8356b00a400
Catherine <cat.taylor.taylor@btinternet.com>
2017-05-29 11:32:01 +0100

    Added in object reader.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,31 @@
 //
 // Began May 2017
 
-int main(){
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <thread>
+#include "Read_Obj.hpp"
+
+int main(int argc, char* argv[] ){
+
+    int width, height;
+	if(argc>1){
+		width = atoi(argv[1]);
+		height = atoi(argv[2]);
+	}
+	else{
+		width=1920;
+		height=1080;
+	}
+
+    float *V, *N, *VT;
+    int F, *FV, *FN, *FT;
+
+    ObjFile mesh_dino("dino_puppet.obj");
+	mesh_dino.get_mesh_data(mesh_dino, &FV, &FN, &FT, &VT, &N, &V, &F);
+	ObjFile::clean_up(V,N, VT, FV, FN, FT);
     return 0;
 }
